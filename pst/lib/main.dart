144ab3a050a86931aef31aa3c3bec4e240cdb4f6
ERIC AHONSU <ahonsueric19@gmail.com>
2025-09-20 20:36:07 +0000

    second committ

--- pst/lib/main.dart
+++ pst/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
-import 'package:pst/features/auth/bloc/auth_bloc.dart';
-import 'package:pst/features/auth/data/auth_repository.dart';
-import 'package:pst/features/business/bloc/business_bloc.dart';
-import 'package:pst/features/business/data/business_repository.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:pst/features/reviews/bloc/review_bloc.dart';
-import 'package:pst/features/reviews/data/review_repository.dart';
+
+import 'firebase_options.dart';
 import 'core/routes/app_router.dart';
+import 'features/auth/bloc/auth_bloc.dart';
+import 'features/auth/data/auth_repository.dart';
+import 'features/business/bloc/business_bloc.dart';
+import 'features/business/data/business_repository.dart';
+import 'features/reviews/bloc/review_bloc.dart';
+import 'features/reviews/data/review_repository.dart';
 
 // Service Locator
 final sl = GetIt.instance;
@@ -19,34 +19,9 @@ final sl = GetIt.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Charger les variables d'environnement
-  await dotenv.load(fileName: ".env");
-
-  // Déterminer les options Firebase en fonction de la plateforme
-  FirebaseOptions firebaseOptions;
-  if (kIsWeb) {
-    firebaseOptions = FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_WEB_API_KEY']!,
-      appId: dotenv.env['FIREBASE_WEB_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_WEB_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_WEB_PROJECT_ID']!,
-      authDomain: dotenv.env['FIREBASE_WEB_AUTH_DOMAIN']!,
-      storageBucket: dotenv.env['FIREBASE_WEB_STORAGE_BUCKET']!,
-    );
-  } else {
-    // Supposons Android pour l'instant, car c'était la seule config fournie
-    firebaseOptions = FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY']!,
-      appId: dotenv.env['FIREBASE_ANDROID_APP_ID']!,
-      messagingSenderId: dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID']!,
-      projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID']!,
-      storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET']!,
-    );
-  }
-
-  // Initialiser Firebase
+  // Initialiser Firebase avec flutterfire configure
   await Firebase.initializeApp(
-    options: firebaseOptions,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   // Configuration de l'injection de dépendances
@@ -61,8 +36,7 @@ void _setupDependencies() {
   sl.registerSingleton<BusinessRepository>(BusinessRepository());
   sl.registerSingleton<ReviewRepository>(ReviewRepository());
 
-  // Blocs
-  // On ne les met pas dans GetIt car leur cycle de vie est géré par flutter_bloc
+  // Blocs : gestion via flutter_bloc, pas besoin de GetIt
 }
 
 class TogoAvisApp extends StatefulWidget {
